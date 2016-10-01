77008cfc7c6cb0a5342c4d81039406184f3d527a
beatscoindev <jose44@iname.com>
2016-10-02 03:40:50 +0500

    PoW End

--- src/main.h
+++ src/main.h
@@ -27,6 +27,8 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
+static const int LAST_POW_BLOCK = 20000;
+
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
